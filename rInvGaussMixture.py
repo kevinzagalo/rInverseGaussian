44517c76a089e97600e7bdc2cfe8b0c1d79d2737
kzagalo <kevinzagalo@me.com>
2022-04-15 16:00:27 +0200

    aitken_acceleration

--- rInvGaussMixture.py
+++ rInvGaussMixture.py
@@ -1,23 +1,25 @@
 import numpy
 from math import sqrt, log, exp
-from scipy.optimize import minimize
+from scipy.optimize import minimize, fmin_bfgs
+from tqdm import tqdm
+from sklearn.mixture._base import BaseMixture
 
 
 class rInvGaussMixture:
 
-    def __init__(self, n_components=None, max_iter=100, tol=1e-4, modes=None, shapes=None, weights=None):
+    def __init__(self, n_components=None, max_iter=100, tol=1e-4, modes_init=None, shapes_init=None, weights_init=None):
+        self.tol = tol,
+        self.n_iter_ = max_iter
+
         if n_components:
             self._n_components = n_components
-        elif modes:
-            self._n_components = len(modes)
+        elif modes_init:
+            self._n_components = len(modes_init)
 
-        self.modes_ = modes
-        self.shapes_ = shapes
-        self.weights_ = weights
+        self.modes_ = modes_init
+        self.shapes_ = shapes_init
+        self.weights_ = weights_init
         self.converged_ = False
-
-        self.n_iter_ = max_iter
-        self.tol = tol
 
     def mu(self, theta, gamma):
         return sqrt(theta * (3 * gamma + theta))
@@ -31,7 +33,7 @@ class rInvGaussMixture:
         return a1 * exp(-a2/2)
 
     def log_invGauss_pdf(self, x, theta, gamma):
-        a1 = log(self.lambd(theta, gamma))/2 - log(2 * numpy.pi * x ** 3)
+        a1 = log(self.lambd(theta, gamma))/2 - log(2) - log(numpy.pi) - 3 * log(x)
         a2 = self.lambd(theta, gamma) * (x - self.mu(theta, gamma)) / (x * self.mu(theta, gamma) ** 2)
         return a1 - a2/2
 
@@ -46,12 +48,12 @@ class rInvGaussMixture:
         dLL_dtheta = - 3/(2*x) \
                      - theta/(x*gamma) \
                      + 1/p + 3*gamma/(2*theta*p) \
-                     + sqrt(theta)/(2*gamma*sqrt(p)) \
-                     + sqrt(p)/(2*gamma*sqrt(theta))
+                     + sqrt(theta/p)/(2*gamma) \
+                     + sqrt(p/theta)/(2*gamma)
         dLL_dgamma = x/(2*gamma**2) \
                      + theta**2/(2*x*gamma**2) \
                      - theta/(2*gamma*p) \
-                     + 3*sqrt(theta)/(2*gamma*sqrt(p)) \
+                     + 3*sqrt(theta/p)/(2*gamma) \
                      - sqrt(theta*p)/gamma**2
         return numpy.array([dLL_dtheta, dLL_dgamma])
 
@@ -65,69 +67,75 @@ class rInvGaussMixture:
                       - 3*sqrt(theta)/(gamma**2*sqrt(p))\
                       + 2*sqrt(theta*p)/gamma**3
         dLL_dtheta_dgamma = theta/(x*gamma**2) \
-                            - (27*gamma**3 + 30*gamma*theta**2 \
-                                + 4*theta**3 \
+                            - (27*gamma**3 + 30*gamma*theta**2\
+                                + 4*theta**3\
                                 + 3*gamma**2 * (21*theta + 2*sqrt(theta*p)))\
                               / (4*gamma**2*sqrt(theta*p**5))
         return numpy.matrix([[dLL_dtheta2, dLL_dtheta_dgamma], [dLL_dtheta_dgamma, dLL_dgamma2]])
 
-    def _complete_likelihood(self, X, z, theta, gamma):
-        return sum([z[i] * self.log_invGauss_pdf(x_i, theta, gamma) for i, x_i in enumerate(X)])
+    def _complete_likelihood(self, X, zz, theta, gamma):
+        return sum([zz[i] * self.log_invGauss_pdf(x_i, theta, gamma) for i, x_i in enumerate(X)])
 
-    def _derivative_complete_likelihood(self, X, z, theta, gamma):
-        return numpy.array([z[i] * self._dlogf(x_i, theta, gamma) for i, x_i in enumerate(X)]).sum(axis=0)
+    def _derivative_complete_likelihood(self, X, zz, theta, gamma):
+        return numpy.array([zz[i] * self._dlogf(x_i, theta, gamma) for i, x_i in enumerate(X)]).sum(axis=0)
 
-    def _second_derivative_complete_likelihood(self, X, z, theta, gamma):
-        return sum([z[i] * self._hesslogf(x_i, theta, gamma) for i, x_i in enumerate(X)])
+    def _second_derivative_complete_likelihood(self, X, zz, theta, gamma):
+        return sum([zz[i] * self._hesslogf(x_i, theta, gamma) for i, x_i in enumerate(X)])
 
-    def _update_weights(self, X, z):
-        z = numpy.zeros(z.shape)
+    def _update_weights(self, X):
+        zz = numpy.zeros((len(X), self._n_components))
         for i, x_i in enumerate(X):
-            z[i, :] = numpy.array(self._proba_components(x_i)) / self.pdf(x_i)
-        return z
+            zz[i, :] = numpy.array(self._proba_components(x_i)) / self.pdf(x_i)
+        return zz
 
-    def _update_params(self, X, z, x0):
-        hess_LL = lambda x: -self._second_derivative_complete_likelihood(X, z, x[0], x[1])
-        grad_LL = lambda x: -self._derivative_complete_likelihood(X, z, x[0], x[1])
-        LL = lambda x: -self._complete_likelihood(X, z, x[0], x[1])
+    def _update_params(self, X, zz, x0):
+        hess_LL = lambda x: -self._second_derivative_complete_likelihood(X, zz, x[0], x[1])
+        grad_LL = lambda x: -self._derivative_complete_likelihood(X, zz, x[0], x[1])
+        LL = lambda x: -self._complete_likelihood(X, zz, x[0], x[1])
         res = minimize(fun=LL, method='Newton-CG', x0=x0, jac=grad_LL, hess=hess_LL)
+        #return fmin_bfgs(f=LL, x0=x0, fprime=grad_LL, disp=False)
         return res['x']
 
     def _score_complete(self, X, z):
-        return sum([sum([z[i, j] * self.log_invGauss_pdf(x_i, self.modes_[j], self.shapes_[j])
+        l1 = sum([sum([z[i, j] * log(pi_j) for j, pi_j in enumerate(self.weights_)]) for i, _ in enumerate(X)])
+        l2 = sum([sum([z[i, j] * self.log_invGauss_pdf(x_i, self.modes_[j], self.shapes_[j])
                          for i, x_i in enumerate(X)]) for j in range(self._n_components)])
+        return l1 + l2
 
     def score_sample(self, X):
         return [sum([self.weights_[j] * self.invGauss_pdf(x_i, self.modes_[j], self.shapes_[j])
                      for j in range(self._n_components)]) for i, x_i in enumerate(X)]
 
-    def score(self, X):
+    def score(self, X, y=None):
         return sum(self.score_sample(X))
 
     def _EM(self, X, verbose=False):
-        z = numpy.ones((len(X), self._n_components))
-        self.weights_ = numpy.mean(z, axis=0)
-        self.modes_ = numpy.random.randint(1, 100, self._n_components)
-        self.shapes_ = [1] * self._n_components
-        likelihood = self._score_complete(X, z)
+        self.weights_ = [1/self._n_components] * self._n_components
+        self.modes_ = [50] * self._n_components
+        self.shapes_ = [1.] * self._n_components
+        likelihood = self.score(X)
         max_iter = self.n_iter_
         old_l = 0
 
-        while not self.converged_ and max_iter > 0:
+        for _ in tqdm(range(self.n_iter_), ascii=True, desc='iterations'):
             max_iter -= 1
             old_likelihood = old_l
             old_l = likelihood
+
             # E-step
-            z = self._update_weights(X, z)
+            z = self._update_weights(X)
 
             # M-step
             self.weights_ = numpy.mean(z, axis=0).tolist()
             for j in range(self._n_components):
-                self.modes_[j], self.shapes_[j] = self._update_params(X, z[:, j], (self.modes_[j], self.shapes_[j]))
+                self.modes_[j], self.shapes_[j] = self._update_params(X, z[:, j],
+                                                                      numpy.array((self.modes_[j], self.shapes_[j])))
 
             likelihood = self._score_complete(X, z)
             aitken_acceleration = (likelihood - old_l) / (old_l - old_likelihood)
             self.converged_ = abs((likelihood - old_l)/(1-aitken_acceleration)) < self.tol
+            #if self.converged_:
+            #    break
 
         if verbose:
             if self.converged_:
@@ -156,6 +164,12 @@ class rInvGaussMixture:
         return self.fit(X).predict(X)
 
     def sample(self, n_sample=1):
+        if n_sample < 1:
+            raise ValueError(
+                "Invalid value for 'n_samples': %d . The sampling requires at "
+                "least one sample." % (self.n_components)
+            )
+
         # https://fr.wikipedia.org/wiki/Loi_inverse-gaussienne#Simulation_num%C3%A9rique_de_la_loi_inverse-gaussienne
         clusters_ = numpy.random.choice(a=range(self._n_components), p=self.weights_, size=n_sample)
         mu = numpy.zeros(n_sample)
@@ -174,16 +188,15 @@ class rInvGaussMixture:
         S[notok] = mu[notok]**2 / X[notok]
         return S
 
-    def get_params(self):
+    def get_parameters(self):
         return {'weights': self.weights_, 'modes': self.modes_,
                 'shapes': self.shapes_, 'n_components': self._n_components}
 
 
 if __name__ == '__main__':
-    sample = rInvGaussMixture(modes=[10, 50], weights=[0.4, 0.6], shapes=[1, 1]).sample(10000)
-    print(sample.shape)
+    sample = rInvGaussMixture(modes_init=[10, 50], weights_init=[0.3, 0.7], shapes_init=[1, 2]).sample(100)
     model = rInvGaussMixture(2).fit(sample, verbose=True)
-    print(model.get_params())
+    print(model.get_parameters())
     #y = model._second_derivative_complete_likelihood(sample, [1]*len(sample), 1, 1)
     #print(y)
 
