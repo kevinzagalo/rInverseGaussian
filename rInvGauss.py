70e11990423ba86a489ab232277de6b95bbfe34c
kzagalo <kevinzagalo@me.com>
2022-10-19 10:53:07 +0200

    mle 1 component

--- rInvGauss.py
+++ rInvGauss.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from numpy import sqrt, log, exp
 from scipy.optimize import minimize, root_scalar
 from tqdm import trange
@@ -41,7 +41,7 @@ class rInvGauss:
         else:  # in case we use the object parameters
             mu = self.mean
             lambd = self.shape
-        a1 = sqrt(lambd / (2 * numpy.pi * x ** 3))
+        a1 = sqrt(lambd / (2 * np.pi * x ** 3))
         a2 = lambd * (x - mu) ** 2 / (x * mu ** 2)
         return a1 * exp(-a2 / 2)
 
@@ -59,7 +59,7 @@ class rInvGauss:
         else:  # in case we use the object parameters
             mu = self.mean
             lambd = self.shape
-        a1 = log(lambd) - log(2) - log(numpy.pi) - 3 * log(x)
+        a1 = log(lambd) - log(2) - log(np.pi) - 3 * log(x)
         a2 = lambd * (x - mu) ** 2 / (x * mu ** 2)
         return a1 / 2 - a2 / 2
 
@@ -81,7 +81,7 @@ class rInvGauss:
                      - mode / (2 * cv * p) \
                      + 1.5 * sqrt(mode) / (cv * sqrt(p)) \
                      - sqrt(mode * p) / cv ** 2
-        return numpy.array([dLL_dmu, dLL_dgamma])
+        return np.array([dLL_dmu, dLL_dgamma])
 
     def _hesslogf(self, x, mode=None, cv=None):
         if mode and cv:
@@ -103,47 +103,22 @@ class rInvGauss:
                                + 4 * mode ** 3 \
                                + 3 * cv ** 2 * (21 * mode + 2 * sqrt(mode * p))) \
                             / (4 * cv ** 2 * sqrt(mode * p ** 5))
-        return numpy.matrix([[dLL_dmu2, dLL_dmu_dgamma], [dLL_dmu_dgamma, dLL_dgamma2]])
+        return np.matrix([[dLL_dmu2, dLL_dmu_dgamma], [dLL_dmu_dgamma, dLL_dgamma2]])
 
     def score(self, X, y=None, mode=None, cv=None):
         return sum([self.log_pdf(x, mode, cv) for x in X])
 
     def _update_params(self, XX, x0):
         hess_LL = lambda y: sum([-self._hesslogf(x, y[0], y[1]) for x in XX])
-        grad_LL = lambda y: numpy.array([-self._dlogf(x, y[0], y[1]) for x in XX]).sum(axis=0)
+        grad_LL = lambda y: np.array([-self._dlogf(x, y[0], y[1]) for x in XX]).sum(axis=0)
         LL = lambda y: -self.score(XX, mode=y[0], cv=y[1])
         res = minimize(fun=LL, method='dogleg', x0=x0, jac=grad_LL, hess=hess_LL)['x']
         return res
 
     def fit(self, XX):
-        X = numpy.array(XX).copy()
-
-        if self.mode is None:
-            self.mode = numpy.mean(X)
-        if self.cv is None:
-            self.cv = 1.
-
-        l2 = self.score(X)
-        max_iter = self.n_iter
-        l1 = 0
-        l2_inf = 0
-        pbar = trange(self.n_iter)
-        for _ in pbar:
-            l0 = l1
-            l1 = l2
-            self.mode, self.cv = self._update_params(X, numpy.array((self.mode, self.cv)))
-            l2 = self.score(X)
-            aitken_acceleration = (l2 - l1) / (l1 - l0) if abs(l1 - l0) > 0 else 0
-            l1_inf = l2_inf
-            l2_inf = l1 + (l2 - l1) / (1 - aitken_acceleration)
-            self.converged_ = abs(l2_inf - l1_inf) < self.tol
-            pbar.set_description('acceleration = {}'.format(aitken_acceleration))
-            if self.converged_:
-                if self.verbose:
-                    print('Converged in {} iterations'.format(self.n_iter - max_iter + 1))
-                return self
-            max_iter -= 1
-        print('Not converged...')
+        X = np.array(XX).copy()
+        self.cv = 1 / np.mean(1/X - 1/X.mean())
+        self.mode = self._mode(X.mean(), X.mean()**2/self.cv)
         return self
 
     def kde(self, X, cv=None):
@@ -151,7 +126,7 @@ class rInvGauss:
             pass
         else:
             cv = self.cv
-        return lambda t: numpy.mean([self.pdf(t, x, cv) for x in X])
+        return lambda t: np.mean([self.pdf(t, x, cv) for x in X])
 
     def sample(self, n_sample=1):
         if n_sample < 1:
@@ -160,12 +135,12 @@ class rInvGauss:
             )
 
         # https://fr.wikipedia.org/wiki/Loi_inverse-gaussienne#Simulation_num%C3%A9rique_de_la_loi_inverse-gaussienne
-        y = numpy.random.normal(size=n_sample) ** 2
+        y = np.random.normal(size=n_sample) ** 2
         X = self.mean + (
-                self.mean ** 2 * y - self.mean * numpy.sqrt(4 * self.mean * self.shape * y + self.mean ** 2 * y ** 2)) / (
+                self.mean ** 2 * y - self.mean * np.sqrt(4 * self.mean * self.shape * y + self.mean ** 2 * y ** 2)) / (
                         2 * self.shape)
-        U = numpy.random.rand(n_sample)
-        S = numpy.zeros(n_sample)
+        U = np.random.rand(n_sample)
+        S = np.zeros(n_sample)
         Z = self.mean / (self.mean + X)
         ok = (U <= Z)
         notok = (U > Z)
